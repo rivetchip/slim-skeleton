ccac0363fedd9397248c8f5ae15e24321785fe60
rivetchip <me@spidery.fr>
2017-07-22 15:17:09 +0200

    add routing with htaccess

--- routes.php
+++ routes.php
@@ -6,16 +6,16 @@ $year = '(19|20)\d\d';
 $all = '[^/]+';
 
 return [
-    'home' => ['get', '/slim-skeleton/', function($request, $response) {
+    'home' => ['get', '/', function($request, $response) {
         return 'home ok';
     }],
 
-    'page' => ['get', "/slim-skeleton/page/($word)/($word)", '\App\Test@hello'],
+    'page' => ['get', "/page/($word)/($word)", '\App\Test@hello'],
 
-    'page.post' => ['post', '/slim-skeleton/page/(\w+)/(\w+)/post', '\App\Test@postHello'],
+    'page.post' => ['post', '/page/(\w+)/(\w+)/post', '\App\Test@postHello'],
 
-    'invoke' => [['get','post'], '/slim-skeleton/invoke', \App\Test::class],
+    'invoke' => [['get','post'], '/invoke', \App\Test::class],
 
-    'upload' => [['get','post'], '/slim-skeleton/upload', '\App\Test@upload']
+    'upload' => [['get','post'], '/upload', '\App\Test@upload']
 ];
 
